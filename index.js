d6b1df8c4bc15ce81a20845c79abf88848c3bec3
Faure Victor <faurevic+github@gmail.com>
2023-12-05 21:20:18 +0100

    Added title and date display

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const width = 700, height = 500
 
-let svg = d3
-    .select("body")
+let svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -9,22 +8,17 @@ let svg = d3
 // Group
 let g = svg.append("g")
 
-let tooltip = d3
-    .select('body')
+let tooltip = d3.select('body')
     .append('div')
     .attr('class', 'hidden tooltip')
 
-const projection = d3
-    .geoConicConformal()
+const projection = d3.geoConicConformal()
     .center([2.454071, 46.279229])
     .scale(2800);
 
-let path = d3
-    .geoPath()
-    .projection(projection);
+let path = d3.geoPath().projection(projection);
 
-const color = d3
-    .scaleQuantize()
+const color = d3.scaleQuantize()
     .range(['#fee5d9','#fcae91','#fb6a4a','#de2d26','#a50f15'])
 
 d3.csv("covid.csv").then((data) => {
@@ -35,6 +29,10 @@ d3.csv("covid.csv").then((data) => {
     ]);
 
     d3.json("departements-version-simplifiee.geojson").then((json) => {
+        let dates = cleanData.reduce((acc, cur) => {
+            if (!acc.includes(cur.jour)) acc.push(cur.jour)
+            return acc
+        }, [])
         for (let i = 0; i < json.features.length; i++) {
             let codeDep = json.features[i].properties.code
             let depData = cleanData.filter((row) => row.dep == codeDep).map((e) => e.hosp)
@@ -42,9 +40,7 @@ d3.csv("covid.csv").then((data) => {
         }
         const drawMap = (day) => {
             map = svg.selectAll("path").data(json.features);
-
-            map
-                .join("path")
+            map.join("path")
                 .attr("d", path)
                 .style("fill", (d) => {
                     let value = d.properties.values[day]
@@ -60,14 +56,16 @@ d3.csv("covid.csv").then((data) => {
                         .attr('style', 'left:' + (mousePosition[0] + 15) + 'px; top:' + (mousePosition[1] - 35) + 'px;')
                         .html(d.properties.nom + ' ' + d.properties.values[day])
                 })
-            d3.select('#day').html("Jour: " + day)
+            date = new Date(dates[day]).toLocaleDateString(undefined, {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+            })
+            d3.select('#day').html(date)
         }
         const slider = document.getElementById('slider')
-        slider.addEventListener('input', (e) => {
-            console.log(`Loading day ${e.target.value}`)
-            drawMap(e.target.value)
-        })
+        slider.addEventListener('input', (e) => drawMap(e.target.value))
         drawMap(slider.value)
     })
 })
-
